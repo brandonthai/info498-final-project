958067758510b3e5b282145820f183a3a82177fd
Brandon <bthaib@uw.edu>
2016-02-26 11:18:26 -0800

    Get data from API based on required perameters (ie. lat/lng)

---
+++ scripts/apiQuery.R
@@ -0,0 +1,17 @@
+library(jsonlite)
+source('scripts/getLatLng.R')
+
+get_data <- function(user_city, user_state) {
+  base_url <- "https://zilyo.p.mashape.com/search?"
+  key_parameter <- "mashape-key="
+  key <- "nowlV7qpGSmshTJ2jv6AeLUAGUrjp1632Pzjsn40XflJySxVK5"
+  latlng <- getLatLng(user_city, user_state)
+  lat <- latlng$lat
+  lng <- latlng$lon
+  
+  required_parameters <- paste0("&latitude=", lat, "&longitude=", lng)
+  
+  query <- paste0(base_url, key_parameter, key, required_parameters)
+  data <- fromJSON(query)
+  return(data)
+}
