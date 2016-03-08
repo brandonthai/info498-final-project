91500f1d27a1c9fb5df7fe5dc52987a6e795a9e0
baronbaura <bbaura@uw.edu>
2016-03-07 16:47:30 -0800

    edited chart 3 as function

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -4,12 +4,18 @@ source('scripts/apiQuery.R')
 
 data <- get_data(queryData)
 
-just_propType <- group_by(data, attr.propType.text) %>%
-  summarise("meanofprice" = mean(price.monthly))
-
-plot_ly(just_propType,
-        x = attr.propType.text,
-        y = meanofprice,
-        type = 'bar') %>%
-  layout(xaxis = list(title = "Property Type"), yaxis = list(title = "Avg. Price"), 
-                      title = "Average Prices for Property Types")
+# function
+get_chart3 <- function(queryData){
+  data <- get_data(queryData)
+  just_propType <- group_by(data, attr.propType.text) %>%
+    summarise("meanofprice" = mean(price.monthly))
+  
+  p <- plot_ly(just_propType,
+               x = attr.propType.text,
+               y = meanofprice,
+               type = 'bar'
+  ) %>%
+    layout(xaxis = list(title = "Property Type"), yaxis = list(title = "Avg. Price"), 
+           title = "Average Prices for Property Types")
+    return(p)
+}
